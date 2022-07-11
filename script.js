d2cad143caa3b614eda28e972502d5471a7ab727
SSulaberidze <ssulberidze@gmail.com>
2022-07-11 16:42:24 +0400

    javascript update

--- script.js
+++ script.js
@@ -1,5 +1,56 @@
 let navigationlinks = document. getElementById ('navigation-bar');
-let burgerbutton = document. getElementById ('togglebutton');
-burgerbutton.addEventListener ('click', function() {
+let burgerbar = document. getElementById ('togglebutton');
+burgerbar.addEventListener ('click', function() {
     navigationlinks.classList.toggle('active');
+})
+document.getElementById('purchase').addEventListener('click', function() {
+    alert('მადლობა ბილეთის შესყიდვისათვის')
+})
+document.getElementById('purchase-1').addEventListener('click', function() {
+    alert('მადლობა ბილეთის შესყიდვისათვის')
+})
+document.getElementById('purchase-2').addEventListener('click', function() {
+    alert('მადლობა ბილეთის შესყიდვისათვის')
+})
+document.getElementById('purchase-3').addEventListener('click', function() {
+    alert('მადლობა ბილეთის შესყიდვისათვის')
+})
+document.getElementById('purchase-4').addEventListener('click', function() {
+    alert('მადლობა ბილეთის შესყიდვისათვის')
+})
+document.getElementById('purchase-5').addEventListener('click', function() {
+    alert('მადლობა ბილეთის შესყიდვისათვის')
+})
+document.getElementById('purchase-6').addEventListener('click', function() {
+    alert('მადლობა ბილეთის შესყიდვისათვის')
+})
+document.getElementById('purchase-7').addEventListener('click', function() {
+    alert('მადლობა ბილეთის შესყიდვისათვის')
+})
+document.getElementById('purchase-8').addEventListener('click', function() {
+    alert('მადლობა ბილეთის შესყიდვისათვის')
+})
+document.getElementById('purchase-9').addEventListener('click', function() {
+    alert('მადლობა ბილეთის შესყიდვისათვის')
+})
+document.getElementById('purchase-10').addEventListener('click', function() {
+    alert('მადლობა ბილეთის შესყიდვისათვის')
+})
+document.getElementById('purchase-11').addEventListener('click', function() {
+    alert('მადლობა ბილეთის შესყიდვისათვის')
+})
+document.getElementById('purchase-12').addEventListener('click', function() {
+    alert('მადლობა ბილეთის შესყიდვისათვის')
+})
+document.getElementById('purchase-13').addEventListener('click', function() {
+    alert('მადლობა ბილეთის შესყიდვისათვის')
+})
+document.getElementById('purchase-14').addEventListener('click', function() {
+    alert('მადლობა ბილეთის შესყიდვისათვის')
+})
+document.getElementById('purchase-15').addEventListener('click', function() {
+    alert('მადლობა ბილეთის შესყიდვისათვის')
+})
+document.getElementById('submit').addEventListener('click', function() {
+    alert ('თქვენი წერილი მიღებულია, მალე განვიხილავთ და გიპასხუხებთ.')
 })
