0a1859f15e7b641a6c607276b7f619e98637e175
SSulaberidze <ssulberidze@gmail.com>
2022-07-15 15:51:43 +0400

    Last major update

--- script.js
+++ script.js
@@ -6,109 +6,54 @@ burgerbar.addEventListener ('click', function() {
 })
 
 document.getElementById('purchase').addEventListener('click', function() {
-    alert('მადლობა ბილეთის შესყიდვისათვის.')
+    alert('მადლობა ბილეთის შესყიდვისათვის. Thank you for purchasing ticket.')
 })
 document.getElementById('purchase-1').addEventListener('click', function() {
-    alert('მადლობა ბილეთის შესყიდვისათვის.')
+    alert('მადლობა ბილეთის შესყიდვისათვის. Thank you for purchasing ticket.')
 })
 document.getElementById('purchase-2').addEventListener('click', function() {
-    alert('მადლობა ბილეთის შესყიდვისათვის.')
+    alert('მადლობა ბილეთის შესყიდვისათვის. Thank you for purchasing ticket.')
 })
 document.getElementById('purchase-3').addEventListener('click', function() {
-    alert('მადლობა ბილეთის შესყიდვისათვის')
+    alert('მადლობა ბილეთის შესყიდვისათვის. Thank you for purchasing ticket.')
 })
 document.getElementById('purchase-4').addEventListener('click', function() {
-    alert('მადლობა ბილეთის შესყიდვისათვის.')
+    alert('მადლობა ბილეთის შესყიდვისათვის. Thank you for purchasing ticket.')
 })
 document.getElementById('purchase-5').addEventListener('click', function() {
-    alert('მადლობა ბილეთის შესყიდვისათვის.')
+    alert('მადლობა ბილეთის შესყიდვისათვის. Thank you for purchasing ticket.')
 })
 document.getElementById('purchase-6').addEventListener('click', function() {
-    alert('მადლობა ბილეთის შესყიდვისათვის.')
+    alert('მადლობა ბილეთის შესყიდვისათვის. Thank you for purchasing ticket.')
 })
 document.getElementById('purchase-7').addEventListener('click', function() {
-    alert('მადლობა ბილეთის შესყიდვისათვის.')
+    alert('მადლობა ბილეთის შესყიდვისათვის. Thank you for purchasing ticket.')
 })
 document.getElementById('purchase-8').addEventListener('click', function() {
-    alert('მადლობა ბილეთის შესყიდვისათვის.')
+    alert('მადლობა ბილეთის შესყიდვისათვის. Thank you for purchasing ticket.')
 })
 document.getElementById('purchase-9').addEventListener('click', function() {
-    alert('მადლობა ბილეთის შესყიდვისათვის')
+    alert('მადლობა ბილეთის შესყიდვისათვის. Thank you for purchasing ticket.')
 })
 document.getElementById('purchase-10').addEventListener('click', function() {
-    alert('მადლობა ბილეთის შესყიდვისათვის.')
+    alert('მადლობა ბილეთის შესყიდვისათვის. Thank you for purchasing ticket.')
 })
 document.getElementById('purchase-11').addEventListener('click', function() {
-    alert('მადლობა ბილეთის შესყიდვისათვის.')
+    alert('მადლობა ბილეთის შესყიდვისათვის. Thank you for purchasing ticket.')
 })
 document.getElementById('purchase-12').addEventListener('click', function() {
-    alert('მადლობა ბილეთის შესყიდვისათვის.')
+    alert('მადლობა ბილეთის შესყიდვისათვის. Thank you for purchasing ticket.')
 })
 document.getElementById('purchase-13').addEventListener('click', function() {
-    alert('მადლობა ბილეთის შესყიდვისათვის.')
+    alert('მადლობა ბილეთის შესყიდვისათვის. Thank you for purchasing ticket.')
 })
 document.getElementById('purchase-14').addEventListener('click', function() {
-    alert('მადლობა ბილეთის შესყიდვისათვის.')
+    alert('მადლობა ბილეთის შესყიდვისათვის. Thank you for purchasing ticket.')
 })
 document.getElementById('purchase-15').addEventListener('click', function() {
-    alert('მადლობა ბილეთის შესყიდვისათვის.')
+    alert('მადლობა ბილეთის შესყიდვისათვის. Thank you for purchasing ticket.')
 })
 
 document.getElementById('submit').addEventListener('click', function() {
-    alert ('თქვენი წერილი მიღებულია, მალე განვიხილავთ და გიპასხუხებთ.')
-})
-
-// english
-
-document.getElementById('purchase-en').addEventListener('click', function() {
-    alert('Thank you for purchasing ticket.')
-})
-document.getElementById('purchase-1-en').addEventListener('click', function() {
-    alert('Thank you for purchasing ticket.')
-})
-document.getElementById('purchase-2-en').addEventListener('click', function() {
-    alert('Thank you for purchasing ticket.')
-})
-document.getElementById('purchase-3-en').addEventListener('click', function() {
-    alert('Thank you for purchasing ticket.')
-})
-document.getElementById('purchase-4-en').addEventListener('click', function() {
-    alert('Thank you for purchasing ticket.')
-})
-document.getElementById('purchase-5-en').addEventListener('click', function() {
-    alert('Thank you for purchasing ticket.')
-})
-document.getElementById('purchase-6-en').addEventListener('click', function() {
-    alert('Thank you for purchasing ticket.')
-})
-document.getElementById('purchase-7-en').addEventListener('click', function() {
-    alert('Thank you for purchasing ticket.')
-})
-document.getElementById('purchase-8-en').addEventListener('click', function() {
-    alert('Thank you for purchasing ticket.')
-})
-document.getElementById('purchase-9-en').addEventListener('click', function() {
-    alert('Thank you for purchasing ticket.')
-})
-document.getElementById('purchase-10-en').addEventListener('click', function() {
-    alert('Thank you for purchasing ticket.')
-})
-document.getElementById('purchase-11-en').addEventListener('click', function() {
-    alert('Thank you for purchasing ticket.')
-})
-document.getElementById('purchase-12-en').addEventListener('click', function() {
-    alert('Thank you for purchasing ticket.')
-})
-document.getElementById('purchase-13-en').addEventListener('click', function() {
-    alert('Thank you for purchasing ticket.')
-})
-document.getElementById('purchase-14-en').addEventListener('click', function() {
-    alert('Thank you for purchasing ticket.')
-})
-document.getElementById('purchase-15-en').addEventListener('click', function() {
-    alert('Thank you for purchasing ticket.')
-})
-
-document.getElementById('submit-en').addEventListener('click', function() {
-    alert ('Your letter has been received, soon we will review it and answer you.')
+    alert ('თქვენი წერილი მიღებულია, მალე განვიხილავთ და გიპასხუხებთ. Your letter has been received, soon we will review it and answer you.')
 })
